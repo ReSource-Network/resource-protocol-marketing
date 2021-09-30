13180845780447cd5edf8d5b60b9f4b7760ba28d
Joseph Sobrero <joesobrero@gmail.com>
2021-09-29 18:46:01 -0700

    add new copy in StakeInRealEconomy

--- src/components/StakeInRealEconomy/StakeInRealEconomy.js
+++ src/components/StakeInRealEconomy/StakeInRealEconomy.js
@@ -5,6 +5,15 @@ import {getCloudinaryImagePath} from "../App/App";
 // animation images
 const cityImgSrcName = 'city-buildings_i3hjgz.svg'
 
+// text
+const paragraphCopy =
+      'ReSource staking rewards stem from the activity of real world businesses -'
+    + ' Brick and mortar shops, SaaS providers, and utility companies.'
+    + ' Even if crypto enters deep winter,'
+    + ' Bitcoin goes sideways and the DeFi space contracts,'
+    + ' ReSource staking pools remain exposed to real enterprises,'
+    + ' with real customers and real products.'
+
 
 class StakeInRealEconomy extends React.Component {
     constructor(props) {
@@ -20,6 +29,10 @@ class StakeInRealEconomy extends React.Component {
                 <h4>
                     { 'Stake into the real economy' }
                 </h4>
+
+                <p>
+                    {paragraphCopy}
+                </p>
 
                 <img
                     src={getCloudinaryImagePath(cityImgSrcName)}
