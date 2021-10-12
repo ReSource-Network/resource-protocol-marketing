15c144910c05985e7243191b6bba2fa6ba7dd9a7
Joseph Sobrero <joesobrero@gmail.com>
2021-10-12 13:07:28 -0700

    duplicate form fixes to StakeInRealEconomy

--- src/components/StakeInRealEconomy/StakeInRealEconomy.js
+++ src/components/StakeInRealEconomy/StakeInRealEconomy.js
@@ -3,11 +3,13 @@ import './StakeInRealEconomy.css';
 import axios from "axios";
 import { customAlphabet } from 'nanoid';
 import { getCloudinaryImagePath } from '../App/App';
+import { useState } from "react";
 
 // for email capture
 const nanoid = customAlphabet("1234567890abcdef", 10);
 const apiKey = process.env.REACT_APP_CIO_API_KEY;
 const siteId = process.env.REACT_APP_CIO_SITE_ID;
+const writeToken = process.env.REACT_APP_SEGMENT_WRITE_KEY;
 
 // animation images
 const cityImgSrcName = 'city-buildings_i3hjgz.svg'
@@ -22,71 +24,72 @@ const paragraphCopy =
     + ' with real customers and real products.'
 
 
-class StakeInRealEconomy extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
+export const StakeInRealEconomy = () => {
+    const [email, setEmail] = useState();
+    const [submitted, setSubmitted] = useState(false);
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleChange(event) {
-        this.setState({ email: event.target.value });
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-
-        const { email } = this.state;
-        return captureFormSubmissionEmail(email);
-    }
-
-    render() {
-        return (
-            <section
-                id={'stakeInRealEconomy'}
-            >
-                <h4>
-                    { 'Stake into the real economy' }
-                </h4>
-
-                <p>
-                    {paragraphCopy}
-                </p>
-
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        id={'email'}
-                        type={'email'}
-                        onChange={this.handleChange}
-                        placeholder={'Enter email'}
-                    />
-                    <input
-                        id={'submit'}
-                        type={'submit'}
-                        value={'Get TGE announcements'}
-                    />
-                </form>
-
-                <img
-                    src={getCloudinaryImagePath(cityImgSrcName)}
-                    alt={'city buildings illustration'}
-                    id={'StakeInRealEconomyCityIllustration'}
-                />
-            </section>
-        )
+    const handleChange = (e) => {
+        const { value } = e.target;
+        const emailState = value.trim().toLowerCase();
+        setEmail(emailState);
     };
 
-}
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await captureFormSubmissionEmail(email);
+            setSubmitted(true);
+            setEmail("");
+        } catch (e) {
+            console.log("Error capturing email: ", e);
+        }
+    };
+
+    return (
+        <section
+            id={'stakeInRealEconomy'}
+        >
+            <h4>
+                { 'Stake into the real economy' }
+            </h4>
+
+            <p>
+                {paragraphCopy}
+            </p>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    id={'email'}
+                    type={'email'}
+                    onChange={handleChange}
+                    placeholder={'Enter email'}
+                />
+                <input
+                    id={'submit'}
+                    type={'submit'}
+                    value={submitted ? 'Submitted!' : 'Get TGE announcements'}
+                />
+            </form>
+
+            <img
+                src={getCloudinaryImagePath(cityImgSrcName)}
+                alt={'city buildings illustration'}
+                id={'StakeInRealEconomyCityIllustration'}
+            />
+        </section>
+    );
+};
 
 // for email capture
 const captureFormSubmissionEmail = async (email) => {
-    const axiosConfig = {
+    const customerIOConfig = {
         method: "POST",
         url: "https://track.customer.io/api/v1/forms/resource_protocol_email/submit",
+        crossDomain: true,
+        mode: "CORS",
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "text/plain",
         },
         auth: {
             username: siteId,
@@ -100,7 +103,33 @@ const captureFormSubmissionEmail = async (email) => {
         }),
     };
 
-    return axios(axiosConfig);
+    const segmentConfig = {
+        method: "POST",
+        url: "https://api.segment.io/v1/track",
+        crossDomain: true,
+        mode: "CORS",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        auth: {
+            username: writeToken,
+            password: "",
+        },
+        data: JSON.stringify({
+            userId: nanoid(),
+            event: "EmailCapture-ResourceProtocolMarketing",
+            properties: {
+                email: email,
+                tags: "ReSource Protocol Marketing Capture",
+            },
+        }),
+    };
+
+    try {
+        return await Promise.all([axios(segmentConfig), axios(customerIOConfig)]);
+    } catch (e) {
+        console.log("Error submitting to segment & customerio: ", e);
+    }
 };
 
 export default StakeInRealEconomy;
