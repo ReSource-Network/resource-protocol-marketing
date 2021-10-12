1e604d6a3d5df606f47575b9abeb10ddcb6bd8f9
Sayer <sayer@worldtree.io>
2021-10-12 11:55:12 -0700

    debug email capture

--- src/components/FutureIsMutual/FutureIsMutual.js
+++ src/components/FutureIsMutual/FutureIsMutual.js
@@ -2,92 +2,89 @@ import "./FutureIsMutual.css";
 import React from "react";
 import axios from "axios";
 import { customAlphabet } from "nanoid";
-import {getCloudinaryImagePath} from "../App/App";
+import { getCloudinaryImagePath } from "../App/App";
 
 // images
-const futureIsMutualImgSrcName = 'future-is-mutual_thynoz.svg'
+const futureIsMutualImgSrcName = "future-is-mutual_thynoz.svg";
 
 // for email capture
 const nanoid = customAlphabet("1234567890abcdef", 10);
 const apiKey = process.env.REACT_APP_CIO_API_KEY;
 const siteId = process.env.REACT_APP_CIO_SITE_ID;
 
-
+console.log("FutureIsMutual.js -- apiKey:", apiKey);
+console.log("FutureIsMutual.js -- siteId:", siteId);
 
 class FutureIsMutual extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { email: "" };
+  constructor(props) {
+    super(props);
+    this.state = { email: "" };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({ email: event.target.value });
-    }
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
+  handleSubmit(event) {
+    event.preventDefault();
 
-        const { email } = this.state.email;
-        return captureFormSubmissionEmail(email);
-    }
+    const { email } = this.state.email;
+    return captureFormSubmissionEmail(email);
+  }
 
-    render() {
-        return (
-            <section id={"futureIsMutual"}>
-                <img
-                    src={getCloudinaryImagePath(futureIsMutualImgSrcName)}
-                    id={"futureIsMutualImg"}
-                    alt={"person walking through space stepping on planets"}
-                />
-                <div id={"futureIsMutualContentContainerContainer"}>
-                    <div id={"futureIsMutualContentContainer"}>
-                        <h4> {"The future is mutual"}
-                        </h4>
+  render() {
+    return (
+      <section id={"futureIsMutual"}>
+        <img
+          src={getCloudinaryImagePath(futureIsMutualImgSrcName)}
+          id={"futureIsMutualImg"}
+          alt={"person walking through space stepping on planets"}
+        />
+        <div id={"futureIsMutualContentContainerContainer"}>
+          <div id={"futureIsMutualContentContainer"}>
+            <h4> {"The future is mutual"}</h4>
 
-                        <form onSubmit={this.handleSubmit}>
-                            <input
-                                id={'email'}
-                                type={'email'}
-                                onChange={this.handleChange}
-                                placeholder={'Enter email'}
-                            />
-                            <input
-                                id={'submit'}
-                                type={'submit'}
-                                value={'Take part'}
-                            />
-                        </form>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+            <form onSubmit={this.handleSubmit}>
+              <input
+                id={"email"}
+                type={"email"}
+                onChange={this.handleChange}
+                placeholder={"Enter email"}
+              />
+              <input id={"submit"} type={"submit"} value={"Take part"} />
+            </form>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 // for email capture
 const captureFormSubmissionEmail = async (email) => {
-    const axiosConfig = {
-        method: "POST",
-        url: "https://track.customer.io/api/v1/forms/resource_protocol_email/submit",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        auth: {
-            username: siteId,
-            password: apiKey,
-        },
-        data: JSON.stringify({
-            data: {
-                email: email,
-                id: nanoid(),
-            },
-        }),
-    };
+  const axiosConfig = {
+    method: "POST",
+    url: "https://track.customer.io/api/v1/forms/resource_protocol_email/submit",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+    },
+    auth: {
+      username: siteId,
+      password: apiKey,
+    },
+    data: JSON.stringify({
+      data: {
+        email: email,
+        id: nanoid(),
+      },
+    }),
+  };
 
-    return axios(axiosConfig);
+  return axios(axiosConfig);
 };
 
 export default FutureIsMutual;
